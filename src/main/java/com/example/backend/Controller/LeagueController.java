fcc97a86d3c4ac28ecaa758562560e2bfe1378e0
Marek_Fryger <marekfryger@o2.pl>
2022-04-27 13:19:18 +0200

    @CrossOrigin added

--- src/main/java/com/example/backend/Controller/LeagueController.java
+++ src/main/java/com/example/backend/Controller/LeagueController.java
@@ -6,16 +6,14 @@ import com.example.backend.Services.LeagueService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/league", produces = {MediaType.APPLICATION_JSON_VALUE})
+@CrossOrigin
 public class LeagueController {
 
 
--- src/main/java/com/example/backend/Controller/MatchController.java
+++ src/main/java/com/example/backend/Controller/MatchController.java
@@ -6,15 +6,13 @@ import com.example.backend.Services.MatchService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/matches", produces = {MediaType.APPLICATION_JSON_VALUE})
+@CrossOrigin
 public class MatchController {
     private final MatchService matchService;
 
